f17758c0f1fe0330192fd8af195a743da9b98d0a
userpc <userpc@mail.com>
2019-01-06 10:12:36 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
-var cards = ["queen","queen","king","king"]
-var cardsInPlay = []
-var cardOne = [0]
-cardsInPlay.push(cardOne)
-console.log("User flipped queen")
-var cardTwo = [2]
-cardsInPlay.push(cardTwo)
-console.log("User flipped king")
-if (cardsInPlay.length === 2) 
-	(cardsInPlay[0] === cardsInPlay[1])
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var checkForMatch = function () {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+}
+};
+var flipCard = function (cardId) {
+	console.log("User flipped " +cards[cardId]);
+	cardsInPlay.push (cards[cardId]);
+if (cardsInPlay.length === 2); 
+	(cardsInPlay[0] === cardsInPlay[1]);
 alert("You found a match!");
-alert("Sorry, try again.")
+alert("Sorry, try again.");	
+checkForMatch();
+};
+flipCard(0);
+flipCard(2);
