66150ea9386d473234c939bea3a055f8cad47a62
userpc <userpc@mail.com>
2019-01-07 08:39:24 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,4 +1,21 @@
-var cards = ["queen","queen","king","king"];
+var cards = [
+{rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{rank: "king",
+suit: "hearts",
+cardImage: "images/kind-of-hearts.png"
+},
+{rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+}
+];
 var cardsInPlay = [];
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -8,8 +25,8 @@ console.log("Sorry, try again.");
 }
 };
 var flipCard = function (cardId) {
-	console.log("User flipped " +cards[cardId]);
-	cardsInPlay.push (cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push (cards[cardId].rank);
 if (cardsInPlay.length === 2); 
 	(cardsInPlay[0] === cardsInPlay[1]);
 alert("You found a match!");
@@ -18,3 +35,5 @@ checkForMatch();
 };
 flipCard(0);
 flipCard(2);
+console.log(cardImage);
+console.log(suit);
